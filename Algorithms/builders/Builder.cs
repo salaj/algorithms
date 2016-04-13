0af2911b8d6c6c32ab4ba60bac7abcd5cb3b3c98
Jakub Sala <evans92@o2.pl>
2016-04-13 15:03:59 +0200

    missing builder

---
+++ Algorithms/builders/Builder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public abstract class Builder
+    {
+        public abstract dynamic Build(dynamic p1, dynamic p2, dynamic p3);
+    }
+}
